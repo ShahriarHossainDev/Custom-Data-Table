9edbb5a73f2b7790d91251f4908bd611c005d65d
ShahriarHossainDev <shahriar.hossain.dev@gmail.com>
2023-04-01 15:00:32 +0600

    Initial Commit

---
+++ Custom Data Table/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Custom Data Table
+//
+//  Created by Shishir_Mac on 1/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
